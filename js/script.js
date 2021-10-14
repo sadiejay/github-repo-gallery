7001c41601a8573b30f0a8ef5ee9f718fce7aa44
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-10-14 15:47:43 -0500

    Fetches and displays user information

--- js/script.js
+++ js/script.js
@@ -9,5 +9,21 @@ const getData = async function (){
     const res = await fetch (`https://api.github.com/users/${username}`);
     const data = await res.json();
     console.log(data);
+    displayInfo(data);
 };
 getData();
+
+const displayInfo = function (data) {
+    let userInfo = document.createElement('div');
+    userInfo.classList.add('user-info');
+    userInfo.innerHTML = `<figure>
+    <img alt="user avatar" src=${data.avatar_url} />
+  </figure>
+  <div>
+    <p><strong>Name:</strong> ${data.name}</p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+  </div>`;
+  overview.append(userInfo);
+  };
