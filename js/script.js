52253c1577eed8c6f40b3228364baf6a6f4bbaad
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-10-14 20:04:55 -0500

    Fetches and lists repos

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ const overview = document.querySelector('.overview');
 // github username
 const username = 'sadiejay';
 
+// selects unordered repo list
+const repoList = document.querySelector('.repo-list');
+
+// defines sort
+const sort = 'updated';
+
+// defines per page
+const perPage = 100;
+
 // fetches github API JSON Data
 const getData = async function (){
     const res = await fetch (`https://api.github.com/users/${username}`);
@@ -13,6 +22,7 @@ const getData = async function (){
 };
 getData();
 
+// displays JSON data in div
 const displayInfo = function (data) {
     let userInfo = document.createElement('div');
     userInfo.classList.add('user-info');
@@ -26,4 +36,25 @@ const displayInfo = function (data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
   overview.append(userInfo);
+  getRepoData();
   };
+
+//  fetches repos with API
+const getRepoData = async function (){
+    // const resRepo = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const resRepo = await fetch (`https://api.github.com/users/${username}/repos?sort=${sort}&per_page=${perPage}`);
+    let dataRepo = await resRepo.json();
+    console.log(dataRepo);
+    repoInfo (dataRepo);
+};
+// getRepoData();
+
+// displays Repo Info
+const repoInfo = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement('li');
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+}
